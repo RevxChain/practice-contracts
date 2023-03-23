ac11b5cab729bf5b39f1c5f7b5b44d4abbc5d7ff
RevxChain <104643965+RevxChain@users.noreply.github.com>
2023-03-23 16:15:12 +0400

    Create IAccessControl.sol

---
+++ token_life_cycle/contracts/lending/access/IAccessControl.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+// OpenZeppelin Contracts v4.4.1 (modified)
+
+pragma solidity ^0.8.0;
+
+interface IAccessControl {
+
+    event RoleAdminChanged(bytes32 indexed role, bytes32 indexed previousAdminRole, bytes32 indexed newAdminRole);
+
+    event RoleGranted(bytes32 indexed role, address indexed account, address indexed sender);
+
+    function hasRole(bytes32 role, address account) external view returns (bool);
+
+    function getRoleAdmin(bytes32 role) external view returns (bytes32);
+
+}
